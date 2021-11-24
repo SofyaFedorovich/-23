c4d1d8bbec27dcb5d04d618fede3c5e66a643b59
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-24 12:18:48 +0300

    Задача

--- Занятие 23/Program.cs
+++ Занятие 23/Program.cs
@@ -10,95 +10,32 @@ namespace Занятие_23
 {
     class Program
     {
-        //    //    static void Main(string[] args)
-        //    //    {
-        //    //        Console.Write("Введите число, факториал которого нужно найти ");
-        //    //        int number = Convert.ToInt32(Console.ReadLine());
-
-        //    //        BigInteger result = FactorialAsync(number).Result;
-        //    //        Console.WriteLine("Факториал числа {0:0,0}", result);
-
-        //    //        Console.ReadKey();
-        //    //    }
-        //    //    static BigInteger Factorial(int number) //метод возаращает значение
-        //    //    {
-        //    //        BigInteger fact = 0;
-        //    //        Console.WriteLine("Идет расчет...");
-        //    //        for (int i = 1; i <= number; i++)
-        //    //        {
-        //    //            fact *= (BigInteger)i;
-        //    //            Thread.Sleep(500);
-        //    //        }
-        //    //        return (fact);
-        //    //    }
-        //    //    static async Task<BigInteger> FactorialAsync(int number)
-        //    //    {
-        //    //        Console.WriteLine(" Расчет запущен: ");
-        //    //        BigInteger result = await Task.Run(() => Factorial(number));
-        //    //        Console.WriteLine(" Расчет закончен");
-        //    //        return result;
-        //    //    }
-        //    //}
-        //    //асинхронный код  для вычисления факториала числа
-        //    static int n;
-        //    static void Main(string[] args)
-        //    {
-        //        Console.WriteLine("Ввести целое число для нахождения факториала: ");
-        //        int n = Convert.ToInt32(Console.ReadLine());
-        //        FactorialAsync();
-
-        //        //BigInteger factorial = FactorialAsync(n).factorial;
-
-        //        //Console.WriteLine($"{n}!={factorial}");
-        //        Console.ReadKey();
-        //    }
-        //    static int Factorial (int n)
-
-        //    {
-        //        int factorial = 1;
-        //        //Console.WriteLine("Начало расчета");
-        //        for (int i = 1; i <= n; i++)
-        //        {
-        //            factorial *= i;
-        //            //Thread.Sleep(100);
-        //        }
-        //        Console.WriteLine($"{n}!={factorial}");
-        //        return factorial;
-
-        //    }
-        //    static async void FactorialAsync()
-
-        //    {
-        //        //Console.WriteLine("Расчет идет");
-        //        await Task.Run(() => FactorialAsync());
-        //        //FactorialAsync factorial = await Task.Run(() => factorial(n));
-        //        //Console.WriteLine("Расчет выполнен");
-        //        //return factorial;
-        //    }
-
-
-        //}
-        static int n;
+        
+        //асинхронный код  для вычисления факториала числа
+        static int a;
         static void Main(string[] args)
         {
-            Console.WriteLine("Ввести целое число для нахождения факториала: ");
-            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввести целое число для вычисления факториала: ");
+            a = Convert.ToInt32(Console.ReadLine());
             FactorialAsync();
             Console.ReadKey();
         }
         static int Factorial()
         {
-            int fac = 1;
-            for (int i = 1; i <= n; i++)
+            int factorial = 1;
+            Console.WriteLine("Расчет выполняется");
+            for (int i = 1; i <= a; i++)
             {
-                fac *= i;
+                factorial *= i;
             }
-            Console.WriteLine($"{n}! = {fac}");
-            return fac;
+            Console.WriteLine($"{a}! = {factorial}");
+            return factorial;
         }
         static async void FactorialAsync()
         {
+            Console.WriteLine("Начало расчета");
             await Task.Run(() => Factorial());
+            Console.WriteLine($"Факториал числа {a} найден!");
         }
     }
 }
