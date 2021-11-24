67b6ca030b7aff7d952ffcf6a209b562e15645ad
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-24 11:55:23 +0300

    Добавьте файлы проекта.

---
+++ Занятие 23/BigInteger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Занятие_23
+{
+    internal class BigInteger
+    {
+        public static explicit operator BigInteger(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
